05208b94671d4490c4042e68879c9a991dee36b6
Gary Dean <garydean@okusi.id>
2023-11-29 06:29:16 +0800

    update 0.4.20(9)

--- README.md
+++ README.md
@@ -24,3 +24,29 @@
     ls *.txt | presuf vi +34
     find -name '*.sh' | presuf "$EDITOR" '+1' -Qs >myscript
     findgrep ~/scripts -name 'bash'  | presuf "$EDITOR" -Qs
+# Module: presuf.bash
+
+## Function: presuf
+
+### Desc:
+  Prepend/Append text to filenames in a stream list.
+
+### Usage:
+    presuf [-options] [prefix] [suffix] [<filenames]
+
+### Options:
+    -z|--squeeze  Do not add separator space to prefix or suffix.
+    -X|--execute  Execute the output as a script.
+    -Q|--quote ['|"]
+                  Wraps filename in quotes.
+                  If 'quote' not specified, defaults to "'"
+    -s|--shebang [shebang]
+                  Inserts !#shebang at top of output.
+                  If 'shebang' not specified, defaults to "$BASH"
+    -V|--version  Print version.
+    -h|--help     This docstring help.
+
+### Examples:
+    ls *.txt | presuf vi +34
+    find -name '*.sh' | presuf "$EDITOR" '+1' -Qs >myscript
+    findgrep ~/scripts -name 'bash'  | presuf "$EDITOR" -Qs
--- presuf.bash
+++ presuf.bash
@@ -26,6 +26,20 @@
 #     find -name '*.sh' | presuf "$EDITOR" '+1' -Qs >myscript
 #     findgrep ~/scripts -name 'bash'  | presuf "$EDITOR" -Qs
 
+_bash_docstring.lite() {
+  # bash_docstring.lite (only reads script docstring, not function docstring within the script.)
+  [[ $0 == 'bash' ]] && return 0
+  while IFS= read -r line; do
+    line=${line#"${line%%[![:space:]]*}"}
+    [[ -z "$line" ]] && continue
+    [[ ${line:0:1} == '#' ]] || break
+    [[ $line == '#' ]] && { echo; continue; }
+    [[ ${line:0:2} == '# '  || ${line:0:2} == '#@' ]] && echo "${line:2}"
+  done <"$0"
+  return 0
+}
+declare -fx 'bash_docstring.lite'
+
 presuf() {
   # Function: presuf
   # Desc: Prepend and append text to each filename in a list.
@@ -52,6 +66,8 @@ presuf() {
         ;;
     -V|--version)   echo "$FUNCNAME $VERSION"; return 0 ;;
     -h|--help)
+        _bash_docstring.lite "$0" "$FUNCNAME" | less -FXRS
+
         # #canonical bash_docstring.lite (only reads script docstring, not function docstring within the script.)
         [[ $0 == 'bash' ]] && return 0
         while IFS= read -r line; do
